0850970a465c934d3754504d6f89ad0fa66a5429
Andres <barca.sc8@gmail.com>
2018-01-26 10:11:23 +0100

    Trimestre

---
+++ src/modelo/Trimestre.java
@@ -0,0 +1,14 @@
+package src.modelo;
+
+public class Trimestre{
+	
+	private int id;
+	private String nombre;
+	private Date fechaInicio;
+	private Date fechaFin;
+	private Alumno alumno;
+	
+	
+	
+
+}
