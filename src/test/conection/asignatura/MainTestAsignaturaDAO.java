a02332d4fa772da088b0769e26bbd8bfa18f996e
LitoPrieto <litolk_15@hotmail.es>
2018-01-26 10:08:28 +0100

    main

--- src/test/conection/asignatura/MainTestAsignaturaDAO.java
+++ src/test/conection/asignatura/MainTestAsignaturaDAO.java
@@ -1,23 +1,18 @@
-package test.conection;
 
-import java.util.List;
-
-import dao.AsignaturaDAO;
-import modelo.Asignatura;
-
-public class MainTestAsignaturaDao {
+public class MainTestAsignaturaDAO {
 
 	public static void main(String[] args) {
-
+		
 		AsignaturaDAO asignaturaDAO = new AsignaturaDAO();
 		Asignatura asignatura = new Asignatura();
 
 		List<Asignatura> resultado = asignaturaDAO.getAll(asignatura);
 
-		System.out.println("SIZE "+resultado.size());
-//		
+		System.out.println("SIZE "+resultado.size());	
 		for (int x = 0; x < resultado.size(); x++) {
 			System.out.println(resultado.get(x).toString());
 		}
+
 	}
+
 }
