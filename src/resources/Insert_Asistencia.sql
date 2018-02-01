c5bf5cd13b9c0f05489d523d802b4a0bc2584f4b
ESDPC <luis2508blanco@gmail.com>
2018-02-01 12:01:23 +0100

    SQL para insertar datos en asistencia

---
+++ src/resources/Insert_Asistencia.sql
@@ -0,0 +1,12 @@
+INSERT INTO aulanosa.asistencia
+(id, fecha, asiste, idAlumno)
+VALUES(1, '2018-12-01', 0, 1);
+INSERT INTO aulanosa.asistencia
+(id, fecha, asiste, idAlumno)
+VALUES(2, '2018-1-02', 1, 2);
+INSERT INTO aulanosa.asistencia
+(id, fecha, asiste, idAlumno)
+VALUES(3, '2018-1-03', 1, 3);
+INSERT INTO aulanosa.asistencia
+(id, fecha, asiste, idAlumno)
+VALUES(4, '2018-01-04', 0, 3);
